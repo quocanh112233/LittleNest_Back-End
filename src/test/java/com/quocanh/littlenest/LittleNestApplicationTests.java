6bfe19fc2d945da7f48e19cf2bb76585de895ba7
quocanh112233 <trannguyenquocanh2004@gmail.com>
2025-09-09 12:32:46 +0700

    Initial commit

---
+++ src/test/java/com/quocanh/littlenest/LittleNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quocanh.littlenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
